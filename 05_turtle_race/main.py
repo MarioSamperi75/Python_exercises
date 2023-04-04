0744c6d18021c7a274669fc669b961fa0e547942
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-04 15:51:18 +0200

    Turtle race: creating multiple instances and random racing

--- 05_turtle_race/main.py
+++ 05_turtle_race/main.py
@@ -1,34 +1,30 @@
+import random
 from turtle import Turtle, Screen
 
-tim = Turtle()
-tim.speed(0)
-
-def move_forward():
-    tim.forward(10)
-
-
-def move_backward():
-    tim.forward(-10)
-
-
-def turn_right():
-    tim.right(10)
-
-
-def turn_left():
-    tim.left(10)
-
-
-def reset_position():
-    tim.reset()
-    tim.clear()
-
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+is_game_on = False
 
 screen = Screen()
-screen.listen()
-screen.onkey(move_forward, "w")
-screen.onkey(key="s", fun=move_backward)
-screen.onkey(key="a", fun=turn_left)
-screen.onkey(key="d", fun=turn_right)
-screen.onkey(key="c", fun=reset_position)
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+
+y_position = -100
+players = []
+
+if user_bet:
+    is_game_on = True
+
+for color in colors:
+    player = Turtle(shape="turtle")
+    player.penup()
+    player.color(color)
+    player.goto(x=-230, y=y_position)
+    y_position += 40
+    players.append(player)
+
+while is_game_on:
+    for player in players:
+        player.forward(random.randint(0, 10))
+
 screen.exitonclick()
