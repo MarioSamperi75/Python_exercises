d97e4daff1f4d7284c10d664611f15bf80b676b7
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-05 15:33:50 +0200

    The Snake: creating an managing scoreboard

--- 06_the_snake/main.py
+++ 06_the_snake/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -18,14 +20,13 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-counter = 0
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    if snake.head.distance(food) < 18:
-        counter += 1
+    if snake.head.distance(food) < 19:
+        scoreboard.update_score()
         food.refresh()
 
 
---
+++ 06_the_snake/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+FONT = ('Courier', 24, 'normal')
+ALIGNMENT = 'center'
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        self.score = 0
+        super().__init__()
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.refresh_scoreboard()
+
+
+    def update_score(self):
+        self.score += 1
+        self.refresh_scoreboard()
+
+    def refresh_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
