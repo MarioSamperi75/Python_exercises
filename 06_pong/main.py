cb7bd516197ec146ef25386fa374218a1cb1d9e7
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-06 10:52:26 +0200

    Pong: refactoring the paddle class, creating the second paddle and move it

--- 06_pong/main.py
+++ 06_pong/main.py
@@ -6,18 +6,20 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.tracer(0)
-player1 = Paddle()
+player_r = Paddle((350, 0))
+player_l = Paddle((-350, 0))
 
 
 screen.listen()
 
-screen.onkey(player1.move_up, "Up")
-screen.onkey(player1.move_down, "Down")
+screen.onkey(player_r.move_up, "Up")
+screen.onkey(player_r.move_down, "Down")
+screen.onkey(player_l.move_up, "w")
+screen.onkey(player_l.move_down, "s")
 
 is_game_on = True
 while is_game_on:
     screen.update()
-
 
 
 screen.exitonclick()
--- 06_pong/paddle.py
+++ 06_pong/paddle.py
@@ -2,15 +2,16 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
-        self.paddle = Turtle('square')
-        self.paddle.penup()
-        self.paddle.color('white')
-        self.paddle.turtlesize(5, 1)
-        self.paddle.goto(350, 0)
+    def __init__(self, position):
+        super().__init__()
+        self.shape('square')
+        self.color('white')
+        self.turtlesize(5, 1)
+        self.penup()
+        self.goto(position)
 
     def move_up(self):
-        self.paddle.sety(self.paddle.ycor() + 20)
+        self.sety(self.ycor() + 20)
 
     def move_down(self):
-        self.paddle.sety(self.paddle.ycor() - 20)
+        self.sety(self.ycor() - 20)
