360fe544463b0ca978edd5776c485d086830375f
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-07 10:53:03 +0200

    The frog: refactoring - creating a cars list - random appearing, position and color

--- 08_the_frog_turtle/car_manager.py
+++ 08_the_frog_turtle/car_manager.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
@@ -5,27 +6,27 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
     def __init__(self):
         super().__init__()
-        self.create_a_car()
-        self.car_speed = STARTING_MOVE_DISTANCE
-
-
+        self.cars = []
 
     def create_a_car(self):
-        self.penup()
-        self.shape("square")
-        self.shapesize(1, 2)
-        self.goto(260, 0)
+        new_car = Turtle()
+        new_car.penup()
+        new_car.car_speed = STARTING_MOVE_DISTANCE
+        new_car.shape("square")
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(1, 2)
+        new_car.goto(260, random.randint(-250, 250))
+        self.cars.append(new_car)
 
+   # def move_car(self):
+   #     self.setx(self.xcor() - self.car_speed)
 
-    def move_car(self):
-        self.setx(self.xcor() - self.car_speed)
-
-    def update_speed(self):
-        self.car_speed += MOVE_INCREMENT
-        print(self.car_speed)
+   # def update_speed(self):
+   #     self.car_speed += MOVE_INCREMENT
+   #     print(self.car_speed)
 
 
 
--- 08_the_frog_turtle/main.py
+++ 08_the_frog_turtle/main.py
@@ -1,5 +1,5 @@
 # create the turtle and the turtle movement
-
+import random
 # create the car and the car movement
 
 # Scoreboard level 1: methode to update
@@ -22,19 +22,22 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 scoreboard = Scoreboard()
-car = CarManager()
+car_manager = CarManager()
 player = Player()
 screen.listen()
 screen.onkey(player.move, 'Up')
 
 game_is_on = True
 while game_is_on:
-    car.move_car()
+    #car_manager.move_cars()
 
     if player.is_winner():
         scoreboard.level_up()
         player.restart()
-        car.update_speed()
+        #car_manager.update_speed()
+
+    if random.randint(1, 10) > 8:
+        car_manager.create_a_car()
 
     time.sleep(0.1)
     screen.update()
