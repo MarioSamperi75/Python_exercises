81caf4a56b8d60e5d078f71d4943b070fab8db37
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-09 08:04:04 +0200

    merge Mail:  complete

--- 10_mail_merge/main.py
+++ 10_mail_merge/main.py
@@ -6,3 +6,14 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Names/invited_names.txt") as file:
+    names_temp = file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+
+for name in names_temp:
+    name = name.strip()
+    with open(f"./Output/ReadyToSend/{name}.txt", mode="w") as file:
+        file.write(letter.replace("[name]", name))
