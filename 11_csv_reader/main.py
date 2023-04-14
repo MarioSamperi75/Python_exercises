ccff9a6bbb0b8f8462eeac17074454c069a9a95e
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-14 14:49:24 +0200

    Pandas: operations with data

--- 11_csv_reader/main.py
+++ 11_csv_reader/main.py
@@ -13,8 +13,51 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+# printing the wole dataframe
 print(data)
 
-print(data["temp"])
+# printing a series
+temp_series = data["temp"]
+print(temp_series)
+
+# converting a dataframe to  dictionary
+print(data.to_dict())
+
+# converting a series to a list
+temp_list = temp_series.to_list()
+print(temp_list)
+
+# average
+average_teperature = sum(temp_list) / len(temp_list)
+print(int(average_teperature))
+# alternative
+average_teperature = temp_series.mean()
+print(int(average_teperature))
+
+# max & min look at https://pandas.pydata.org/docs/reference/series.html
+print(temp_series.max())
+print(temp_series.min())
+
+# key notation or object notation - does not matter!
+print(data['temp'])
+print(data.temp)
+
+# select one or more rows
+print(data[data.day == "Monday"])
+print(data[data.condition == "Sunny"])
+
+# print the row with the max temp
+print(data[data.temp == data.temp.max()])
+
+#convert to farenheit
+monday = data[data.day == "Monday"]
+temp_fahrenheit = int(monday.temp) * 9/5 + 32
+print(temp_fahrenheit)
+
+
+
+
+
+
 
 
