8a4a91c3a4d7d9bc20d2c98d605e16186ec37685
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-15 23:07:17 +0200

    us states game: starting project - finding click coordinates

--- 12_us-states-game/main.py
+++ 12_us-states-game/main.py
@@ -7,6 +7,10 @@ screen.addshape(image)
 turtle.shape(image)
 
 
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+turtle.onscreenclick(get_mouse_click_coor)
 
 # screen.exitonclick()
 # alternative:
