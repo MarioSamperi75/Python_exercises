763c2f70ab565238d1a4528a6cb0c978c0f9be9a
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-15 22:37:00 +0200

    Pandas: squirrel-exercise

--- 11_csv_reader/main.py
+++ 11_csv_reader/main.py
@@ -10,49 +10,92 @@
 #
 # print(temperatures)
 
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# # printing the wole dataframe
+# print(data)
+#
+# # printing a series
+# temp_series = data["temp"]
+# print(temp_series)
+#
+# # converting a dataframe to  dictionary
+# print(data.to_dict())
+#
+# # converting a series to a list
+# temp_list = temp_series.to_list()
+# print(temp_list)
+#
+# # average
+# average_teperature = sum(temp_list) / len(temp_list)
+# print(int(average_teperature))
+# # alternative
+# average_teperature = temp_series.mean()
+# print(int(average_teperature))
+#
+# # max & min look at https://pandas.pydata.org/docs/reference/series.html
+# print(temp_series.max())
+# print(temp_series.min())
+#
+# # key notation or object notation - does not matter!
+# print(data['temp'])
+# print(data.temp)
+#
+# # select one or more rows
+# print(data[data.day == "Monday"])
+# print(data[data.condition == "Sunny"])
+#
+# # print the row with the max temp
+# print(data[data.temp == data.temp.max()])
+#
+# #convert to farenheit
+# monday = data[data.day == "Monday"]
+# temp_fahrenheit = int(monday.temp) * 9/5 + 32
+# print(temp_fahrenheit)
+#
+# # Createind a dataframe from scratch
+# data_dict = {
+#     "students": ["Mario", "Ellen", "Sofia", "Alexander"],
+#     "scores": [30, 80, 100, 100]
+# }
+# data = pandas.DataFrame(data_dict)
+# print(data)
+#
+# # creating CSV file from
+# data.to_csv("new_data.csv")
+
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-# printing the wole dataframe
-print(data)
+data = pandas.read_csv("squirrel_data.csv")
 
-# printing a series
-temp_series = data["temp"]
-print(temp_series)
+fur_color_series = data["Primary Fur Color"]
 
-# converting a dataframe to  dictionary
-print(data.to_dict())
+gray_ones = data[data["Primary Fur Color"] == "Gray"]
+total_gray = len(gray_ones)
+# total_gray = gray_ones.get("Primary Fur Color").count()
 
-# converting a series to a list
-temp_list = temp_series.to_list()
-print(temp_list)
+black_ones = data[data["Primary Fur Color"] == "Black"]
+total_black = len(black_ones)
+# total_black = black_ones.get("Primary Fur Color").count()
 
-# average
-average_teperature = sum(temp_list) / len(temp_list)
-print(int(average_teperature))
-# alternative
-average_teperature = temp_series.mean()
-print(int(average_teperature))
+red_ones = data[data["Primary Fur Color"] == "Cinnamon"]
+total_red = len(red_ones)
+# total_red = red_ones.get("Primary Fur Color").count()
 
-# max & min look at https://pandas.pydata.org/docs/reference/series.html
-print(temp_series.max())
-print(temp_series.min())
+data_dict = {
+    "fur_color": ["Grey", "Black", "Red"],
+    "count": [total_gray, total_black, total_red]
+}
 
-# key notation or object notation - does not matter!
-print(data['temp'])
-print(data.temp)
+data = pandas.DataFrame(data_dict)
 
-# select one or more rows
-print(data[data.day == "Monday"])
-print(data[data.condition == "Sunny"])
+data.to_csv("squirrel_count.csv")
 
-# print the row with the max temp
-print(data[data.temp == data.temp.max()])
 
-#convert to farenheit
-monday = data[data.day == "Monday"]
-temp_fahrenheit = int(monday.temp) * 9/5 + 32
-print(temp_fahrenheit)
+
+
+
 
 
 
