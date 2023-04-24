a05cae510ee86648b9bf016c50cf38e2fa521253
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-24 10:48:17 +0200

    Constructor with kvargs

--- 24_gui_and_arguents/playground.py
+++ 24_gui_and_arguents/playground.py
@@ -1,8 +1,34 @@
-def add(*args):
-    tot = 0
-    for arg in args:
-        tot += arg
-    return tot
+# def add(*args):
+#     tot = 0
+#     for arg in args:
+#         tot += arg
+#     return tot
+#
+#
+# print(add(2, 3, 4, 5, 6))
+
+def calculate(n, **kvargs):
+    print(kvargs)
+    # different syntax, the first one allows None as value
+    n += kvargs.get('add')
+    n *= kvargs['multiply']
+    print(n)
 
 
-print(add(2, 3, 4, 5, 6))
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kvarg):
+        self.make = kvarg.get('make')
+        self.model = kvarg.get('model')
+        self.color = kvarg.get("color")
+
+my_car = Car(make="Fiat", model='500')
+print(my_car.color)
+print(my_car.make)
+
+
+
+
