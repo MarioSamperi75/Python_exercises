391ea9d6ae30350c9f7270a897242fcbe5ee12c4
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-24 11:00:40 +0200

    tkinter: working with labels

--- 24_gui_and_arguents/main.py
+++ 24_gui_and_arguents/main.py
@@ -6,8 +6,14 @@ windows.minsize(width=800, height=600)
 
 # label
 my_label = tkinter.Label(text="I am a label", font=("Garamond", 24, "bold"))
-
 my_label.pack()
+my_label['text'] = "New Text"
+my_label
+my_label.config(text='New Text2', background='yellow')
+# more commands? check https://tcl.tk/man/tcl8.6/TkCmd/label.htm
+
+
+
 
 
 
