b099fcee6b70c4245de45206fe05ceb938d70893
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-24 13:17:15 +0200

    tkinter: listend to a button

--- 24_gui_and_arguents/main.py
+++ 24_gui_and_arguents/main.py
@@ -1,18 +1,26 @@
 import tkinter
 
-windows = tkinter.Tk()
+from tkinter import *
+
+windows = Tk()
 windows.title("Mi first GUI program")
 windows.minsize(width=800, height=600)
 
 # label
-my_label = tkinter.Label(text="I am a label", font=("Garamond", 24, "bold"))
+my_label = Label(text="I am a label", font=("Garamond", 24, "bold"))
 my_label.pack()
 my_label['text'] = "New Text"
 my_label
 my_label.config(text='New Text2', background='yellow')
 # more commands? check https://tcl.tk/man/tcl8.6/TkCmd/label.htm
 
+def try_button_listener():
+    print("listener works")
+    my_label['text'] = "Button got clicked"
 
+
+my_button = Button(text="CLIKKA QUI", command=try_button_listener)
+my_button.pack()
 
 
 
