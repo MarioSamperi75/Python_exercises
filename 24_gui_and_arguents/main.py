6734dbbe7e72a33b70e23aa4ab8fe4306b92249f
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-24 16:03:31 +0200

    tkinter: pack, place and grid

--- 24_gui_and_arguents/main.py
+++ 24_gui_and_arguents/main.py
@@ -2,19 +2,6 @@ import tkinter
 
 from tkinter import *
 
-windows = Tk()
-windows.title("Mi first GUI program")
-windows.minsize(width=800, height=600)
-
-# label
-my_label = Label(text="I am a label", font=("Garamond", 24, "bold"))
-my_label.pack()
-my_label['text'] = "New Text"
-my_label
-my_label.config(text='New Text2', background='yellow')
-# more commands? check https://tcl.tk/man/tcl8.6/TkCmd/label.htm
-
-
 def try_button_listener():
     # print("listener works")
     # my_label['text'] = "Button got clicked"
@@ -22,11 +9,33 @@ def try_button_listener():
     my_label['text'] = my_input
 
 
+windows = Tk()
+windows.title("Mi first GUI program")
+windows.minsize(width=80, height=60)
+# windows.config(padx=100, pady=100)
+
+# label
+my_label = Label(text="I am a label", font=("Garamond", 24, "bold"))
+my_label['text'] = "New Text"
+my_label.config(text='New Text2', background='yellow')
+# my_label.pack()
+my_label.grid(row=0, column=0)
+my_label.config(pady=50, padx=50)
+# more commands? check https://tcl.tk/man/tcl8.6/TkCmd/label.htm
+
+# button
 my_button = Button(text="CLIKKA QUI", command=try_button_listener)
-my_button.pack()
+# my_button.pack()
+# my_button.pack(side='left')
+# my_button.place(x=300, y=300)
+my_button.grid(row=1, column=1)
 
+new_button = Button(text="Submit")
+new_button.grid(row=0, column=2)
+
+# entry
 my_entry = Entry(width=10)
-my_entry.pack()
-
+# my_entry.pack()
+my_entry.grid(row=2, column=3)
 
 windows.mainloop()
