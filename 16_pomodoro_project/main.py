244c9296562173b4eaf8ef38d1748919e9ee6f03
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-25 15:14:06 +0200

    Pomodoro: completing the GUI

--- 16_pomodoro_project/main.py
+++ 16_pomodoro_project/main.py
@@ -22,9 +22,21 @@ windows.config(pady=50, padx=100, bg=YELLOW)
 
 pom_img = PhotoImage(file="tomato.png")
 
-canvas = Canvas(width=200, height=224, bg=YELLOW)
-canvas.create_image(102, 112, image=pom_img)
+title_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, 'bold'))
+title_label.grid(row=0, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=pom_img)
 canvas.create_text(104, 132, text="00:00", fill="white", font=("Garamond", 30, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+start_btn = Button(text="Start")
+start_btn.grid(row=2, column=0)
+
+reset_btn = Button(text="Reset")
+reset_btn.grid(row=2, column=2)
+
+check_label = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_label.grid(row=3, column=1)
 
 windows.mainloop()
