d41c5d224c4db0046e5c7c9ab91e0880259467f7
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-26 22:57:16 +0200

    Pomodoro: reset

--- 16_pomodoro_project/main.py
+++ 16_pomodoro_project/main.py
@@ -10,9 +10,20 @@ WORK_MIN = 4
 SHORT_BREAK_MIN = 2
 LONG_BREAK_MIN = 10
 reps = 0
-checks = ""
+timer = None
+# checks = ""
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset():
+    global timer
+    global reps
+    windows.after_cancel(timer)
+    canvas.itemconfig(timer_display, text="00:00")
+    title_label.config(text="Timer")
+    check_label.config(text="")
+    reps = 0
+
 
 
 # ---------------------------- TIMER MECHANISM --------------------------- #
@@ -33,6 +44,7 @@ def start():
 # ---------------------------- COUNTDOWN MECHANISM ------------------------ #
 def count_down(count):
     # global checks
+    global timer
     marks = ""
     minutes = math.floor(count/60)
     seconds = count % 60
@@ -40,10 +52,10 @@ def count_down(count):
     if seconds < 10:
         seconds = f"0{seconds}"
 
-    canvas.itemconfig(timer, text=f"{minutes}:{seconds}")
+    canvas.itemconfig(timer_display, text=f"{minutes}:{seconds}")
 
     if count > 0:
-        windows.after(1000, count_down, count - 1)
+        timer = windows.after(1000, count_down, count - 1)
     else:
         start()
         # if reps % 2 == 0:
@@ -68,14 +80,14 @@ title_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=pom_img)
-timer = canvas.create_text(103, 132, text="00:00", fill="white", font=("Garamond", 30, "bold"))
+timer_display = canvas.create_text(103, 132, text="00:00", fill="white", font=("Garamond", 30, "bold"))
 canvas.grid(row=1, column=1)
 
 
 start_btn = Button(text="Start", command=start)
 start_btn.grid(row=2, column=0)
 
-reset_btn = Button(text="Reset")
+reset_btn = Button(text="Reset", command=reset)
 reset_btn.grid(row=2, column=2)
 
 check_label = Label(text="", bg=YELLOW, fg=GREEN)
