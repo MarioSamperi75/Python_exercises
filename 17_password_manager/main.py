feab83d13d2b5d26a8f499603da00ae44d4e0ea0
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-27 14:19:35 +0200

    Password Manager: enable geterate password button and show the password in the entry

--- 17_password_manager/main.py
+++ 17_password_manager/main.py
@@ -6,36 +6,38 @@ import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 #Password Generator Project
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-# password_list = []
-# [password_list.append(random.choice(letters)) for _ in range(nr_letters)]
-# [password_list.append(random.choice(symbols)) for _ in range(nr_symbols)]
-# [password_list.append(random.choice(numbers)) for _ in range(nr_numbers)]
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-# or
+    # password_list = []
+    # [password_list.append(random.choice(letters)) for _ in range(nr_letters)]
+    # [password_list.append(random.choice(symbols)) for _ in range(nr_symbols)]
+    # [password_list.append(random.choice(numbers)) for _ in range(nr_numbers)]
 
-random_letters = [random.choice(letters) for _ in range(nr_letters)]
-random_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-random_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    # or
 
-password_list = random_letters + random_numbers + random_symbols
+    random_letters = [random.choice(letters) for _ in range(nr_letters)]
+    random_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    random_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
 
-random.shuffle(password_list)
+    password_list = random_letters + random_numbers + random_symbols
 
-# random_password = ""
-# for char in password_list:
-#     random_password += char
+    random.shuffle(password_list)
 
-random_password = "".join(password_list)
+    # random_password = ""
+    # for char in password_list:r
+    #     random_password += char
 
-print(f"Your password is: {random_password}")
+    random_password = "".join(password_list)
+
+    password_entry.insert(0, random_password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -90,7 +92,7 @@ password_label = Label(text="Password")
 password_label.grid(row=3, column=0)
 password_entry = Entry(width=33)
 password_entry.grid(row=3, column=1)
-password_btn = Button(text="Generate Password")
+password_btn = Button(text="Generate Password", command=generate_password)
 password_btn.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=44, command=save)
