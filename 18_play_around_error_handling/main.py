836eaeed7332c5ff668e5ed04432f211584a26fe
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-27 23:35:47 +0200

    Exception handling: try - except - else - finally

--- 18_play_around_error_handling/main.py
+++ 18_play_around_error_handling/main.py
@@ -15,8 +15,24 @@
 # fruit_list[3]
 
 # TypeError
-text = "abc"
-print(text + 5)
+# text = "abc"
+# print(text + 5)
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed")
 
 
 
