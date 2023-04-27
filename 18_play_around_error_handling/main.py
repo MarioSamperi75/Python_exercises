c7108f23a666efe670c7855218ee2e38de90652b
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-27 23:55:59 +0200

    Exception handling: raise an error

--- 18_play_around_error_handling/main.py
+++ 18_play_around_error_handling/main.py
@@ -33,6 +33,20 @@ else:
 finally:
     file.close()
     print("File was closed")
+    # raise TypeError("This is an error that I made up.")
+
+
+# BMI Example
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
 
 
 
