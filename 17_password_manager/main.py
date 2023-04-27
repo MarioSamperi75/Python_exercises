6812ed8a10c3de7280b0e34ff4bfda68754f6a66
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-27 11:12:23 +0200

    Password Manager: saving entries to a text file

--- 17_password_manager/main.py
+++ 17_password_manager/main.py
@@ -4,6 +4,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    f = open("data.txt", "a")
+    f.write(f"{website_entry.get()} | {username_entry.get()} | {password_entry.get()}\n")
+    f.close()
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 windows = Tk()
 windows.title("Password Manager")
@@ -18,10 +30,12 @@ website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=52)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 
 username_label = Label(text="Email/Username:")
 username_label.grid(row=2, column=0)
 username_entry = Entry(width=52)
+username_entry.insert(0, "samperimario75@gmail.com")
 username_entry.grid(row=2, column=1, columnspan=2)
 
 password_label = Label(text="Password")
@@ -31,7 +45,7 @@ password_entry.grid(row=3, column=1)
 password_btn = Button(text="Generate Password")
 password_btn.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=44)
+add_button = Button(text="Add", width=44, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
