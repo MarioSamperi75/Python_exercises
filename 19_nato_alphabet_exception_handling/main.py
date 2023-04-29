54022cc1684b197b10d6d6ba4118741377ebb861
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-04-29 23:33:41 +0200

    Handling exeption: recursive solution

--- 19_nato_alphabet_exception_handling/main.py
+++ 19_nato_alphabet_exception_handling/main.py
@@ -5,16 +5,18 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-valid = False
-while (not valid):
+
+def spell():
     word = input("Enter a word: ").upper()
     try:
         output_list = [phonetic_dict[letter] for letter in word]
     except KeyError:
         print("Sorry, only letters in the alphabet, please")
-        valid = False
+        spell()
     else:
         print(output_list)
-        valid = True
+
+
+spell()
 
 
