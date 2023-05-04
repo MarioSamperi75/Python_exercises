924ae7662d12fa25393ac2b2770620f7b3cf3dae
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-05-04 12:48:41 +0200

    Languages Flash Cards App: Gui  - flip the card after 3 seconds - tkinter windows.after

--- 21_language_flash_cards/main.py
+++ 21_language_flash_cards/main.py
@@ -4,47 +4,50 @@ import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-def on_press_right():
-    next_card()
-
-
-def on_press_wrong():
-    next_card()
-
 
 def next_card():
-    #random_index = random.randrange(len(data.index))
-    #french_word = words_dict[random_index]['French']
-    #english_word = words_dict[random_index]['English']
-    random_card = random.choice(words_dict)
-    french_word = random_card['French']
-    english_word = random_card['English']
-    canvas.itemconfig(language, text='French')
-    canvas.itemconfig(word, text=french_word)
+    global timer
+    global current_card
+    windows.after_cancel(timer)
 
+    current_card = random.choice(words_dict)
+    french_word = current_card['French']
+
+    canvas.itemconfig(image, image=cart_front_img)
+    canvas.itemconfig(language, text='French', fill="black")
+    canvas.itemconfig(word, text=french_word, fill="black")
+    timer = windows.after(3000, show_back)
+
+
+def show_back():
+    canvas.itemconfig(image, image=cart_back_img)
+    english_word = current_card['English']
+    canvas.itemconfig(language, text='English', fill="white")
+    canvas.itemconfig(word, text=english_word, fill="white")
 
 
 data = pandas.read_csv("data/french_words.csv")
 words_dict = data.to_dict(orient="records")
+current_card = {}
 windows = Tk()
 windows.title("Flashy")
 windows.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 cart_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=cart_front_img)
+cart_back_img = PhotoImage(file="images/card_back.png")
+image = canvas.create_image(400, 263, image=cart_front_img)
 language = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
 word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 right_img = PhotoImage(file="images/right.png")
-right_btn = Button(image=right_img, highlightthickness=0, command=on_press_right)
+right_btn = Button(image=right_img, highlightthickness=0, command=next_card)
 right_btn.grid(row=1, column=0)
 
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_btn = Button(image=wrong_img, highlightthickness=0, command=on_press_wrong)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_btn.grid(row=1, column=1)
-
+timer = windows.after(3000, show_back)
 next_card()
-
 windows.mainloop()
