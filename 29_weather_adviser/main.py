ad383bc3ce89bc52f7f655375c507e0a4c8f040b
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-05-25 11:16:25 +0200

    Rain_adviser: sending a sms

--- 29_weather_adviser/main.py
+++ 29_weather_adviser/main.py
@@ -1,8 +1,16 @@
-import requests as requests
+import os
 
-#LATITUDE = 18.193813
-#LONGITUDE = 15.554015
-API_KEY = "69f04e4613056b159c2761a9d9e664d2"
+import requests as requests
+from twilio.http.http_client import TwilioHttpClient
+from twilio.rest import Client
+
+# LATITUDE = 18.193813
+# LONGITUDE = 15.554015
+API_KEY = "---"  # from https://openweathermap.org/
+ACCOUNT_SID = "---"  # from Twilio
+AUTH_TOKEN = "---"  # from Twilio
+TWILIO_TEL_NUM = "---"
+VALID_TEL_NUM = "000123456"  # to validate in twilio account
 
 weather_params = {
     "lat": 71.193813,
@@ -11,23 +19,28 @@ weather_params = {
     "exclude": "current,minutely,daily"
 }
 
-
-#url = f"https://api.openweathermap.org/data/2.5/onecall?lat={LATITUDE}&lon={LONGITUDE}&appid={API_KEY}"
+# url = f"https://api.openweathermap.org/data/2.5/onecall?lat={LATITUDE}&lon={LONGITUDE}&appid={API_KEY}"
 url = "https://api.openweathermap.org/data/2.5/onecall"
 
 response = requests.get(url=url, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 
+will_rain = False
 weather_12h = weather_data['hourly'][:12]
 for hour in weather_12h:
     condition_code = hour['weather'][0]['id']
+    will_rain = True
 
-    if condition_code < 700:
-        print("Prendi l'ombrello")
-        break
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
-
-
-
-
+    client = Client(ACCOUNT_SID, AUTH_TOKEN, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
