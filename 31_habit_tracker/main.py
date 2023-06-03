538cd64c53afed14cc99ac6d1235efd6916470c3
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-03 06:14:47 +0200

    Pixela: Creating a new pixel

--- 31_habit_tracker/main.py
+++ 31_habit_tracker/main.py
@@ -14,6 +14,21 @@ user_params = {
     "notMinor": "yes"
 }
 
-response = requests.post(pixela_endpoint, json=user_params)
+# response = requests.post(pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Python graph",
+    "unit": "min",
+    "type": "int",
+    "color": "ajisai",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+response = requests.post(url=pixela_endpoint, json=graph_config)
 
 print(response.text)
