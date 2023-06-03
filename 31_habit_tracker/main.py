4ba4d19a68d382cadc7043847134d325948891f5
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2023-06-03 06:15:14 +0200

    Pixela: Creating a new pixel

--- 31_habit_tracker/main.py
+++ 31_habit_tracker/main.py
@@ -4,31 +4,48 @@ import requests
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-USERNAME = os.getenv("USERNAME")
+USERNAME = os.getenv("NOMEUTENTE")
 pixela_endpoint = "https://pixe.la/v1/users"
+graph_id = "graph1"
 
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
-}
 
+# user_params = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes"
+# }
+#
 # response = requests.post(pixela_endpoint, json=user_params)
 # print(response.text)
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+# -------------------------------------------------------------------
 
-graph_config = {
-    "id": "graph1",
-    "name": "Python graph",
-    "unit": "min",
-    "type": "int",
-    "color": "ajisai",
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
+# graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+#
+# graph_config = {
+#     "id": "graph1",
+#     "name": "Python graph",
+#     "unit": "min",
+#     "type": "int",
+#     "color": "ajisai"
+# }
+#
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+#
+# response = requests.post(url=graph_endpoint, headers=headers, json=graph_config)
+
+post_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_id}"
+
+post_pixel_config = {
+    "date": "20230422",
+    "quantity": "15",
 }
 
-response = requests.post(url=pixela_endpoint, json=graph_config)
+response = requests.post(url=post_pixel_endpoint, headers=headers, json=post_pixel_config)
+
 
 print(response.text)
+
